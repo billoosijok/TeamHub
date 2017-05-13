4c75f0d0b5ca4a932067be30e6fd1c731701b292
billoosijok <billoosijok@gmail.com>
2017-05-13 12:22:19 -0700

    Init Commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+
+if(isset($_SESSION['user_info'])) { 
+	
+	header("Location: home.php");
+
+} else { 
+	
+	header("Location: login.php");
+	
+}
---
+++ login.php
@@ -0,0 +1,34 @@
+<?php 
+	require 'inc/process_login.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php 
+	$jqueryLink = "libs/js/jquery-2.2.3.min.js";
+	$cssLink = "libs/css/style.css";
+?>
+	<meta charset="UTF-8">
+	<title>Login</title>
+	<meta name="viewport" content="width=device-width, initial-scale=0.9, maximum-scale=1, user-scalable=no">
+	
+	<link rel="stylesheet" type="text/css" href="<?php echo $cssLink; ?>">
+	<script src="<?php echo $jqueryLink ?>"></script>
+	<script src="libs/js/validate_login.js"></script>
+	
+</head>
+<body class="login-page">
+<div id="pagewrapper">
+	<div id="welcome">Login</div>
+	<div id="login">
+		<form method="post" action="">
+			<input type="text" name="login_id" id="loginIdField" placeholder="Email" />
+			<input type="password" name="password" id="passwordField" placeholder="Password" />
+			<p id="status"><?php if(isset($error)){echo $error;}else{echo "&nbsp;";} ?></p>
+			<input type="submit" name="login" value="Log in" />
+		</form>
+	</div>
+</div>
+</body>
+</html>
+
