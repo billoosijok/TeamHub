0b247d4162ba667e165f6adade54e88b471a7420
Chris McGuire <C.MCGUIRE2@labs.clark.edu>
2017-05-24 13:45:21 -0700

    Login and Create account rough drafts
    Added php files for login and create_account. Also css files for both
    pages

---
+++ create_account.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="UTF-8">
+	<title>Create Account</title>
+	<link href="css/create_account.css" rel="stylesheet">
+		
+</head>
+<body>
+<div id="pagewrapper">
+	<div id="content">
+		<div id="createAccountBlock">
+			<h1 id="createAccountHeading">LET'S GET YOU READY TO REVIEW YOUR PEERS</h1>
+		</div><!--create account block-->
+		<form id="createAccountForm">
+			<input type="text" class="createAccountTextBox" id="createEmail" name="" placeholder="Email" /></br>
+			<input type="text" class="createAccountTextBox" id="createFirstName" name="" placeholder="First Name" /></br>
+			<input type="text" class="createAccountTextBox" id="createLastname" name="" placeholder="Last Name" /></br>
+			<input type="text" class="createAccountTextBox" id="createPassword" name="" placeholder="Password" /></br>
+			<input type="text" class="createAccountTextBox" id="reEnterPassword" name="" placeholder="Re-Enter Password" /></br>
+			<input type="submit" id="submitAccount" name="submit" value="Sign In" />
+		</form>
+	<div><!--end content
+</div><!--end pagewrapper
+</body>
+</html>
--- login.php
+++ login.php
@@ -4,20 +4,31 @@
 
 	<meta charset="UTF-8">
 	<title>Login</title>
+	<link href="css/login.css" rel="stylesheet">
 		
 </head>
 <body class="login-page">
 <div id="pagewrapper">
-	<div id="welcome">Login</div>
-	<div id="login">
-		<form method="post" action="">
-			<input type="text" name="login_id" id="loginIdField" placeholder="Email" />
-			<input type="password" name="password" id="passwordField" placeholder="Password" />
-			<p id="status"><?php if(isset($error)){echo $error;}else{echo "&nbsp;";} ?></p>
-			<input type="submit" name="login" value="Log in" />
-		</form>
-	</div>
-</div>
+	<div id="content">
+	
+	<div id="topSquare">
+	
+		<h1 id="mainTitle">TEAMHUB</h1>
+		<h2 id="subTitle">Peer To Peer Review</h2>		
+	</div><!-- end topSquare -->
+	<!--<div id="welcome">Login</div>-->
+		<div id="login">
+			<form method="post" action="">
+				<input type="text" name="login_id" id="loginIdField" placeholder="Email" />
+				</br>
+				<input type="password" name="password" id="passwordField" placeholder="Password" />
+				<p id="status"><?php if(isset($error)){echo $error;}else{echo "&nbsp;";} ?></p>
+				<input type="submit" id="loginButton" name="login" value="sign in" />
+				<a href="create_account.php" id="createAccountLink">CREATE ACCOUNT</a>
+			</form>
+		</div><!-- End login -->
+	</div><!-- End content -->
+</div><!-- End pagewrapper -->
 </body>
 </html>
 
