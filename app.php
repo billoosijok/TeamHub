edefe6fabd69dc3be373d1c4c27c9ecd3ace7c5e
billoosijok <billoosijok@gmail.com>
2017-05-31 13:00:00 -0700

    adds dummy users into their own file

--- app.php
+++ app.php
@@ -5,18 +5,6 @@ require_once "database/connect.php";
 
 $home_url = "http://localhost/teamhub";
 
-$john = [
-	'id' => 2,
-	'first_name' => 'John',
-	'last_name' => 'Smith',
-	'email' => 'john@email.com',
-];
+include "dummy_users.php";
 
-$bruce = [
-	'id' 		=> 1,
-	'first_name' => 'Bruce',
-	'last_name' => 'Elgort',
-	'email'	 	=> 'bruce@email.com',
-];
-
-$_SESSION['user_info'] = $bruce;
+$_SESSION['user_info'] = $matt;
